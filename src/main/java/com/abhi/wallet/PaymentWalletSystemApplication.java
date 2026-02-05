b836fe3e3c5921a2c16db569a23e400cb09f7a8b
Student abhishek45 <abhijadhav0619@gmail.com>
2026-02-05 13:34:22 +0530

    Add main Spring Boot application class for Payment Wallet System

---
+++ src/main/java/com/abhi/wallet/PaymentWalletSystemApplication.java
@@ -0,0 +1,12 @@
+package com.abhi.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentWalletSystemApplication.class, args);
+	}
+}
