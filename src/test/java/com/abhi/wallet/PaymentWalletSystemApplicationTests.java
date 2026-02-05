974d63e0afe4374a87bf628c21d780966e928464
Student abhishek45 <abhijadhav0619@gmail.com>
2026-02-05 13:34:46 +0530

    Add Spring Boot application test class with context loading test

---
+++ src/test/java/com/abhi/wallet/PaymentWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
